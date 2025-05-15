0a8c252e5608c8af0581e53f08ed22059b4d3405
Anirudh2244 <anirudhdubey2244@gmail.com>
2025-05-15 12:47:33 +0530

    logo in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,12 @@ import React from "react";
 function Navbar() {
   return (
     <div className="px-5 sm:px-15 flex justify-between items-center py-2 border-b-2 border-zinc-400">
-      <div className="text-xl sm:text-3xl head-font">Candid Dreams</div>
+      <div className="text-xl sm:text-3xl head-font">
+        <img
+          src="https://i.ibb.co/F4FxYZPL/logo-removebg-preview.png"
+          alt="Candid Dreams" className="h-auto w-30 sm:w-40"
+        />
+      </div>
       <a
         href="https://wa.me/918114412845"
         target="_blank"
